f66ac12467d309061060d542a8547846a4b1f37a
Solaiman Shadin <solaimanshadin@gmail.com>
2020-04-04 23:58:35 +0600

    All file added

---
+++ index.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+require('dotenv').config();
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const uri = process.env.DB_PATH;
+console.log("uri to show", uri);
+let client = new MongoClient(uri ,{useNewUrlParser:true})
+
+app.get('/products' , (req,res) => {
+    client = new MongoClient(uri,{useNewUrlParser:true})
+    client.connect((err) => {
+        const collection = client.db('onlineStore').collection('products');
+        collection.find().toArray((err, documents) => {
+            if(err){
+                console.log(err);
+            }else{
+                res.send(documents);
+            }
+        })
+    })
+})
+app.get('/' , (req,res) => {
+    res.send("Welcome to Ema-Jon Backend Server!")
+})
+app.get('/product/:id', (req,res) => {
+    client = new MongoClient(uri,{useNewUrlParser:true})
+    client.connect(err => {
+        const collection = client.db('onlineStore').collection('products');
+        collection.find({key:req.params.id}).toArray((err, documents) => {
+            if(err){
+                console.log(err);
+            }else{
+                res.send(documents[0]);
+            }
+        })
+    })
+})
+// Post Method
+app.post('/getProductsByKey', (req,res) => {
+    client = new MongoClient(uri,{useNewUrlParser:true})
+    const productKeys = req.body;
+    console.log(productKeys);
+    client.connect(err => {
+        const collection = client.db('onlineStore').collection('products');
+        collection.find({key:{$in:productKeys}}).toArray((err, documents) => {
+            if(err){
+                console.log(err);
+            }else{
+                res.send(documents);
+            }
+        })
+    })
+})
+app.post('/addproduct', (req,res) => {
+    client = new MongoClient(uri,{useNewUrlParser:true})
+    client.connect(err => {
+        const collection = client.db('onlineStore').collection('products');
+        collection.insertOne(req.body , (err, result) => {
+            if(err){
+                console.log(err);
+                res.status(500).send(err)
+
+            }else{
+                res.send(result);
+                console.log(result);
+            }
+            client.close();
+        });
+    })
+
+})
+
+
+const port = 8000;
+app.listen(port , (err) => {
+    console.log("Listing to Port " , port);
+})
